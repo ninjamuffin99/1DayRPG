81fb0c480732580d745ff577bb4d58e919218997
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-05-21 17:34:17 -0700

    renamed files and added simple movement

--- source/Player.hx
+++ source/Player.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.system.FlxAssets.FlxGraphicAsset;
@@ -10,18 +12,45 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
  */
 class Player extends FlxSprite 
 {
+	private var speed:Float = 200;
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
 		
-		var tex = FlxAtlasFrames.fromSparrow(AssetPaths.all_moving_animation__png, AssetPaths.all_moving_animation__xml);
+		var tex = FlxAtlasFrames.fromSparrow(AssetPaths.player__png, AssetPaths.player__xml);
 		frames = tex;
 		
-		animation.addByPrefix("walkL", "Symbol 1", 24);
+		animation.addByPrefix("walkL", "walk left right", 24);
 		animation.play("walkL");
 		
 		setGraphicSize(Std.int(width * 0.2));
+		
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFdacingFlip(FlxObject.RIGHT, true, false);
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		controls();
+		
+		super.update(elapsed);
+	}
+	
+	private function controls():Void
+	{
+		if (FlxG.keys.pressed.A)
+		{
+			velocity.x = -speed;
+			facing = FlxObject.RIGHT;
+		}
+		else if (FlxG.keys.pressed.D)
+		{
+			velocity.x = speed;
+			facing = FlxObject.RIGHT;
+		}
+		else
+			velocity.x = 0;
 	}
 	
 }
